5ad0ab8388323d33eb3e522ff7cddf9f62c88b28
patilpankaj212 <patilpankaj212@gmail.com>
2020-09-01 19:25:18 +0530

    Added Queen and Knight classes - Implementation pending

---
+++ chessPuzzle/src/main/java/chessPuzzle/pieces/Knight.java
@@ -0,0 +1,11 @@
+package chessPuzzle.pieces;
+
+public class Knight extends ChessPiece {
+
+	@Override
+	public String[] getAllValidCells(String currentPosition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ chessPuzzle/src/main/java/chessPuzzle/pieces/Queen.java
@@ -0,0 +1,11 @@
+package chessPuzzle.pieces;
+
+public class Queen extends ChessPiece {
+
+	@Override
+	public String[] getAllValidCells(String currentPosition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
